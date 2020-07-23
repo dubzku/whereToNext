4c397f91ce54987a616c0f2d7169a4d3ca639ac6
Winnie Ku <winn.ku@gmail.com>
2020-07-23 19:29:52 -0400

    :muscle: cleaned up script file and made changes to scrollTop code

--- scripts/script.js
+++ scripts/script.js
@@ -1,126 +1,168 @@
+// Object with the possibilities of results! 
 const destinations = [
     {
         destName: "Newfoundland",
         travelType: "adventure",
-        distance: "close"
+        distance: "close",
+    },
+    {
+        destName: "Utah",
+        travelType: "adventure",
+        distance: "close",
     },
     {
         destName: "Costa Rica",
         travelType: "adventure",
-        distance: "middle"
+        distance: "middle",
+    },
+    {
+        destName: "Bolivia",
+        travelType: "adventure",
+        distance: "middle",
     },
     {
         destName: "Nepal",
         travelType: "adventure",
-        distance: "far"
+        distance: "far",
+    },
+    {
+        destName: "Chile",
+        travelType: "adventure",
+        distance: "far",
     },
     {
         destName: "Mexico",
         travelType: "beach",
-        distance: "close"
+        distance: "close",
+    },
+    {
+        destName: "Miami",
+        travelType: "beach",
+        distance: "close",
     },
     {
         destName: "Belize",
         travelType: "beach",
-        distance: "middle"
+        distance: "middle",
+    },
+    {
+        destName: "Croatia",
+        travelType: "beach",
+        distance: "middle",
     },
     {
         destName: "Australia",
         travelType: "beach",
-        distance: "far"
+        distance: "far",
+    },
+    {
+        destName: "Indonesia",
+        travelType: "beach",
+        distance: "far",
     },
     {
         destName: "New York",
         travelType: "city",
-        distance: "close"
+        distance: "close",
+    },
+    {
+        destName: "San Francisco",
+        travelType: "city",
+        distance: "close",
     },
     {
         destName: "Vienna",
         travelType: "city",
-        distance: "middle"
+        distance: "middle",
+    },
+    {
+        destName: "Amsterdam",
+        travelType: "city",
+        distance: "middle",
     },
     {
         destName: "Taipei",
         travelType: "city",
-        distance: "far"
+        distance: "far",
+    },
+    {
+        destName: "Seoul",
+        travelType: "city",
+        distance: "far",
     },
     {
         destName: "Nova Scotia",
         travelType: "food",
-        distance: "close"
+        distance: "close",
+    },
+    {
+        destName: "New Orleans",
+        travelType: "food",
+        distance: "close",
     },
     {
         destName: "Italy",
         travelType: "food",
-        distance: "middle"
+        distance: "middle",
+    },
+    {
+        destName: "Paris",
+        travelType: "food",
+        distance: "middle",
     },
     {
         destName: "Vietnam",
         travelType: "food",
-        distance: "far"
+        distance: "far",
+    },
+    {
+        destName: "Singapore",
+        travelType: "food",
+        distance: "far",
     }
 ]
 
 // Namespace
 const travelQuiz = {};
 
+// Document Ready
+$(document).ready(function(){
+    travelQuiz.init();
+})
+
 // Init Function
 travelQuiz.init = function() {
-    travelQuiz.formSubmit();
-    travelQuiz.scrollEffects();
+    travelQuiz.coolScrollEffects();
+    travelQuiz.quizSubmit();
 }
 
-travelQuiz.scrollEffects = function() {
-    $("#startQuiz").on('click', function() {
-        const position = $("#questionOne").offset().top;
-        $("html, body").animate({
-            scrollTop: position
-        }, 1000);
-    });
+// Helper Function to give back a random item from a given array
+travelQuiz.arrayRandomizer = function(arrayBucket) {
+    const index = Math.floor(Math.random() * arrayBucket.length);
+    return arrayBucket[index];
+}
 
-    $("#goToQuestionTwo").on('click', function() {
-        const position = $("#questionTwo").offset().top;
-        $("html, body").animate({
-            scrollTop: position
-        }, 1000);
+travelQuiz.coolScrollEffects = function() {
+    $('.scroll').click(function (event) {
+        event.preventDefault();
+        $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 800);
     });
-
-    $("#goToQuestionThree").on('click', function() {
-        const position = $("#questionThree").offset().top;
-        $("html, body").animate({
-            scrollTop: position
-        }, 1000);
-    });
-
-    $("#goToQuestionFour").on('click', function() {
-        const position = $("#questionFour").offset().top;
-        $("html, body").animate({
-            scrollTop: position
-        }, 1000);
-    });
-
-    $("#goToQuestionFive").on('click', function() {
-        const position = $("#questionFive").offset().top;
-        $("html, body").animate({
-            scrollTop: position
-        }, 1000);
-    });
-
+    
     $("#goToResult").on('click', function() {
         const position = $("#results").offset().top;
         $("html, body").animate({
             scrollTop: position
-        }, 1000);
+        }, 800);
     });
 
     $("#top").on('click', function() {
         $("html, body").animate({
             scrollTop: 0
-        }, 1000);
+        }, 800);
     });
 }
 
-travelQuiz.formSubmit = function () {
+travelQuiz.quizSubmit = function () {
     $('form').on('submit', function(event) {
         event.preventDefault();
         const userChoices = {};
@@ -130,13 +172,10 @@ travelQuiz.formSubmit = function () {
         const resultDestination = destinations.filter( (place) => {
             return place.travelType === userChoices.travelType && place.distance === userChoices.distance
         });
+        console.log('results: ', resultDestination);
+        const resultToDisplay = travelQuiz.arrayRandomizer(resultDestination);
+        console.log(resultToDisplay.destName);
 
-        $('.results').html(`<h2>${resultDestination[0].destName}</h2>`);
+        $('.resultsContainer').html(`<h2>${resultToDisplay.destName}</h2>`);
     })
 }
-
-
-//Document Ready
-$(document).ready(function(){
-    travelQuiz.init();
-})
