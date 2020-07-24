fb6bedd744df8f3078a2567e6b9ad8ef1cdfb5ef
Winnie Ku <winn.ku@gmail.com>
2020-07-23 22:38:42 -0400

    :no_entry_sign::white_check_mark: error handling

--- scripts/script.js
+++ scripts/script.js
@@ -142,17 +142,68 @@ travelQuiz.arrayRandomizer = function(arrayBucket) {
     return arrayBucket[index];
 }
 
+// Function for the scrolling effects with error handling included
 travelQuiz.coolScrollEffects = function() {
-    $('.scroll').click(function (event) {
-        event.preventDefault();
-        $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 800);
-    });
-    
-    $("#goToResult").on('click', function() {
-        const position = $("#results").offset().top;
+    $(".startQuiz").on('click', function() {
+        const position = $("#questionOne").offset().top;
         $("html, body").animate({
             scrollTop: position
         }, 800);
+    });
+
+    $(".goToQuestionTwo").on('click', function() {
+        if (!$('input[name=travelType]:checked').val()) {
+            $('p.errorMessage1').text(`You forgot to answer!`);
+        } else {
+            const position = $("#questionTwo").offset().top;
+            $("html, body").animate({
+                scrollTop: position
+            }, 800);
+        }
+    });
+
+    $(".goToQuestionThree").on('click', function() {
+        if (!$('input[name=companions]:checked').val()) {
+            $('p.errorMessage2').text(`You forgot to answer!`);
+        } else {
+            const position = $("#questionThree").offset().top;
+            $("html, body").animate({
+                scrollTop: position
+            }, 800);
+        }
+    });
+
+    $(".goToQuestionFour").on('click', function() {
+        if (!$('input[name=interests]:checked').val()) {
+            $('p.errorMessage3').text(`You forgot to answer!`);
+        } else {
+            const position = $("#questionFour").offset().top;
+            $("html, body").animate({
+                scrollTop: position
+            }, 800);
+        }
+    });
+
+    $(".goToQuestionFive").on('click', function() {
+        if (!$('input[name=distance]:checked').val()) {
+            $('p.errorMessage4').text(`You forgot to answer!`);
+        } else {
+            const position = $("#questionFive").offset().top;
+            $("html, body").animate({
+                scrollTop: position
+            }, 800);
+        }
+    });
+    
+    $("#goToResult").on('click', function() {
+        if (!$('input[name=tripLength]:checked').val()) {
+            $('p.errorMessage5').text(`You forgot to answer!`);
+        } else {
+            const position = $("#results").offset().top;
+            $("html, body").animate({
+                scrollTop: position
+            }, 800);
+        }
     });
 
     $("#top").on('click', function() {
@@ -162,20 +213,35 @@ travelQuiz.coolScrollEffects = function() {
     });
 }
 
+// Function for when user submits their quiz answers
 travelQuiz.quizSubmit = function () {
     $('form').on('submit', function(event) {
         event.preventDefault();
+
+        // Store user's answers in an object called userChoices
         const userChoices = {};
         userChoices.travelType = $('input[name=travelType]:checked').val();
         userChoices.distance = $('input[name=distance]:checked').val();
+        userChoices.companions = $('input[name=companions]:checked').val();
+        userChoices.interests = $('input[name=interests]:checked').val();
+        userChoices.tripLength = $('input[name=tripLength]:checked').val();
 
-        const resultDestination = destinations.filter( (place) => {
-            return place.travelType === userChoices.travelType && place.distance === userChoices.distance
-        });
-        console.log('results: ', resultDestination);
-        const resultToDisplay = travelQuiz.arrayRandomizer(resultDestination);
-        console.log(resultToDisplay.destName);
+        if (!userChoices.travelType || !userChoices.distance || !userChoices.companions || !userChoices.interests || !userChoices.tripLength ) {
+            $('.resultsContainer').html(`<h2>You forgot to fill in some fields!</h2>`);
+        } else {
+            // Filter through the destinations object and return results that match the condition of same travelType AND same distance
 
-        $('.resultsContainer').html(`<h2>${resultToDisplay.destName}</h2>`);
+            // Store the results in a new array called possibleDestinations
+            const possibleDestinations = destinations.filter( (possibleResult) => {
+            return possibleResult.travelType === userChoices.travelType && possibleResult.distance === userChoices.distance;
+            });
+
+            // Use the randomizer helper function to pick a final result from the list of possible results
+            const finalDestination = travelQuiz.arrayRandomizer(possibleDestinations);
+
+            // Show the results in the DOM! 
+            $('.resultsContainer').html(`<h2>${finalDestination.destName}</h2>`);
+        }
+
     })
 }
